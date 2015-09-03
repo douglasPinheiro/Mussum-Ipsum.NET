efc6279c4390f20b8c7486ab2dc54ef645ff189b
Douglas da Costa <douglaspinheiroti@gmail.com>
2015-09-03 12:05:50 -0300

    test for paragraphs number

---
+++ MussumIpsum.Tests/Unit/MussumGeneratorTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MussumIpsum.App;
+using MussumIpsum.App.Enum;
+using System.Text.RegularExpressions;
+
+namespace MussumIpsum.Tests.Unit
+{
+    [TestClass]
+    public class MussumGeneratorTest
+    {
+        [TestMethod]
+        public void GetRandomParagraphs_TestNumberParagraphs()
+        {
+            var paragraphsNumber = new int[] { 1, 3, 5, 8, 10 };
+
+            foreach (var number in paragraphsNumber)
+	        {
+                string text = MussumGenerator.GetRandomParagraphs(ParagraphSize.Any, number);
+
+                int paragraphsNumberResult = Regex.Matches(text, "\n\n").Count + 1;
+
+                Assert.AreEqual(paragraphsNumberResult, number);
+	        }
+        }
+    }
+}
